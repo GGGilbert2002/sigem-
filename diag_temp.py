8e0726f31ad199c279e135b4f7b35c3b2284b394
GGGilbert2002 <aokunoman@gmail.com>
2026-07-16 17:31:21 -0400

    Eliminar archivos temporales de diagnostico

--- diag_temp.py
+++
@@ -1,7 +0,0 @@
-import folium  
-print("Version folium:", folium.__version__)  
-m = folium.Map(location=[11.4, -69.6], cdn_resources="inline")  
-html = m.get_root().render()  
-print("Tamano HTML:", len(html))  
-print("Contiene L.map(:", "L.map(" in html)  
-print("Contiene cdn.jsdelivr:", "cdn.jsdelivr" in html) 
